2931fd6f3b13e7d078421dc5e129e8b8c1cd5765
LEO <LEO@DESKTOP-3798V6K>
2024-10-06 16:58:03 -0300

    analisis

--- analisis_de_datos.py
+++ analisis_de_datos.py
@@ -1,3 +1,9 @@
 
-print("hi")
+import pandas as pd
 
+# df = pd.read_csv("GGAL-rfx-19-7_2-8.csv")
+# print(df)
+df = pd.DataFrame({ "SUJETO":["1", "2", "3"], 
+                "CIRCUNFERENCIA DE CINTURA": ["102", "89", "142"],
+                "IMC": ["29", "26", "35"]})
+print(df)
