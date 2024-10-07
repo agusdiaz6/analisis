1a8ecdea6fb2c3e65665986625d035f842798de0
agusdiaz6 <agus06.ad@gmail.com>
2024-10-07 16:28:44 -0300

    analisis

--- analisis_de_datos.py
+++ analisis_de_datos.py
@@ -3,7 +3,14 @@ import pandas as pd
 
 # df = pd.read_csv("GGAL-rfx-19-7_2-8.csv")
 # print(df)
-df = pd.DataFrame({ "SUJETO":["1", "2", "3"], 
-                "CIRCUNFERENCIA DE CINTURA": ["102", "89", "142"],
-                "IMC": ["29", "26", "35"]})
+#df = pd.DataFrame({ "SUJETO":["1", "2", "3"], 
+ #               "CIRCUNFERENCIA DE CINTURA": ["102", "89", "142"],
+  #              "IMC": ["29", "26", "35"]})
+#print(df)
+#archivo = "/home/genaro/Documentos/agus/titanic.xlsx"
+#df = pd.read_excel(archivo)
+#print(df)
+
+
+df = pd.read_csv("galicia.csv")
 print(df)
